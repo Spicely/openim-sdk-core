a5d473ea295577d3b423f9af0b79c6d9402becb7
Spicely <Spicely@outlook.com>
2023-07-26 18:34:55 +0800

    增加文件上传

--- main/file.go
+++ main/file.go
@@ -1,7 +1,10 @@
 package main
 
 import "C"
-import "open_im_sdk/open_im_sdk"
+import (
+	"encoding/json"
+	"open_im_sdk/open_im_sdk"
+)
 
 type UploadFileCallback struct {
 	operationID string
@@ -9,35 +12,71 @@ type UploadFileCallback struct {
 }
 
 func (u *UploadFileCallback) Open(size int64) {
-	callBack("Open", u.operationID, u.methodName, size, nil)
+	data := map[string]int64{"size": size}
+	jsonData, _ := json.Marshal(data)
+	callBack("Open", u.operationID, u.methodName, nil, jsonData)
 }
 
 func (u *UploadFileCallback) PartSize(partSize int64, num int) {
-	callBack("PartSize", u.operationID, u.methodName, nil, nil)
+	data := map[string]interface{}{
+		"partSize": partSize,
+		"num":      num,
+	}
+	jsonData, _ := json.Marshal(data)
+	callBack("PartSize", u.operationID, u.methodName, nil, jsonData)
 }
 
 func (u *UploadFileCallback) HashPartProgress(index int, size int64, partHash string) {
-	callBack("HashPartProgress", u.operationID, u.methodName, nil, nil)
+	data := map[string]interface{}{
+		"index":    index,
+		"size":     size,
+		"partHash": partHash,
+	}
+	jsonData, _ := json.Marshal(data)
+	callBack("HashPartProgress", u.operationID, u.methodName, nil, jsonData)
 }
 
 func (u *UploadFileCallback) HashPartComplete(partsHash string, fileHash string) {
-	callBack("HashPartComplete", u.operationID, u.methodName, nil, nil)
+	data := map[string]interface{}{
+		"fileHash":  fileHash,
+		"partsHash": partsHash,
+	}
+	jsonData, _ := json.Marshal(data)
+	callBack("HashPartComplete", u.operationID, u.methodName, nil, jsonData)
 }
 
 func (u *UploadFileCallback) UploadID(uploadID string) {
-	callBack("UploadID", u.operationID, u.methodName, nil, nil)
+	callBack("UploadID", u.operationID, u.methodName, nil, uploadID)
 }
 
 func (u *UploadFileCallback) UploadPartComplete(index int, partSize int64, partHash string) {
-	callBack("UploadPartComplete", u.operationID, u.methodName, nil, nil)
+	data := map[string]interface{}{
+		"index":    index,
+		"partSize": partSize,
+		"partHash": partHash,
+	}
+	jsonData, _ := json.Marshal(data)
+	callBack("UploadPartComplete", u.operationID, u.methodName, nil, jsonData)
 }
 
 func (u *UploadFileCallback) UploadComplete(fileSize int64, streamSize int64, storageSize int64) {
-	callBack("UploadComplete", u.operationID, u.methodName, nil, nil)
+	data := map[string]interface{}{
+		"fileSize":    fileSize,
+		"streamSize":  streamSize,
+		"storageSize": storageSize,
+	}
+	jsonData, _ := json.Marshal(data)
+	callBack("UploadComplete", u.operationID, u.methodName, nil, jsonData)
 }
 
 func (u *UploadFileCallback) Complete(size int64, url string, typ int) {
-	callBack("Complete", u.operationID, u.methodName, nil, nil)
+	data := map[string]interface{}{
+		"size": size,
+		"url":  url,
+		"typ":  typ,
+	}
+	jsonData, _ := json.Marshal(data)
+	callBack("Complete", u.operationID, u.methodName, nil, jsonData)
 
 }
 
